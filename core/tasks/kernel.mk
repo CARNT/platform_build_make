202f3f41253ea51185300a4b85af75887a891518
Michele Bono <bono.michele94@gmail.com>
2018-04-14 15:00:37 +0200

    kernel.mk: Get rid of URLs in Clang version string
    Device kernel must have:
    https://github.com/Miccia94/kernel_xiaomi_msm8998/commit/fe4042f0f021b5960a1f851f631ccc2a2c07c56d
    https://github.com/Miccia94/kernel_xiaomi_msm8998/commit/642e39769780c4d4da9dc310c215deb22f94881b
    https://github.com/Miccia94/kernel_xiaomi_msm8998/commit/5594f02c5288303e089b689d61d3828cc52d6f8c
    
    Change-Id: If924924c648baaf80f942a2adc4bd698b0e131c6
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -267,6 +267,8 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     # Find the clang-* directory containing the specified version
     KERNEL_CLANG_VERSION := clang-stable
     TARGET_KERNEL_CLANG_PATH ?= $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
+    KBUILD_COMPILER_STRING := $(shell $(TARGET_KERNEL_CLANG_PATH)/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+    export KBUILD_COMPILER_STRING
     ifeq ($(KERNEL_ARCH),arm64)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
     else ifeq ($(KERNEL_ARCH),arm)
